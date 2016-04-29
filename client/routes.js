74fd1923292bcc6b208571fa76e6d4376b0d570a
Granite Games <gallant.luke33@gmail.com>
2016-04-29 15:39:50 -0400

    Added Events Editor

--- client/routes.js
+++ client/routes.js
@@ -1,29 +1,72 @@
 //Main Pages (Content)
+
 FlowRouter.route('/', {
+  action: function(){
+    BlazeLayout.render("splashLayout", {content:"splashPage"});
+  }
+
+})
+FlowRouter.route('/home', {
   action: function() {
     BlazeLayout.render("mainLayout", {content: "homePage"});
   }
 
 });
+FlowRouter.route('/beer', {
+  action: function() {
+    BlazeLayout.render("mainLayout", {content: "beerPage"});
+  }
+
+});
+FlowRouter.route('/menu', {
+  action: function() {
+    BlazeLayout.render("mainLayout", {content: "menuPage"});
+  }
+
+});
+FlowRouter.route('/events', {
+  action: function() {
+    BlazeLayout.render("mainLayout", {content: "eventsPage"});
+  }
+
+});
+FlowRouter.route('/about', {
+  action: function() {
+    BlazeLayout.render("mainLayout", {content: "aboutPage"});
+  }
+
+});
+
+//Live menu Render
+FlowRouter.route('/livemenu', {
+  action: function(){
+    BlazeLayout.render("liveMenuLayout", {content: "liveMenu"});
+  }
+})
 
 //Admin Routes(CMS)
 FlowRouter.route('/admin', {
   action: function() {
-    BlazeLayout.render("mainLayout", {content: "adminHome"});
+    BlazeLayout.render("adminLayout", {content: "adminHome"});
   }
 });
 FlowRouter.route('/admin/beer',{
   action: function(){
-    BlazeLayout.render("mainLayout", {content: "beerEdit"});
+    BlazeLayout.render("adminLayout", {content: "beerEdit"});
   }
 });
 FlowRouter.route('/admin/food',{
   action: function(){
-    BlazeLayout.render("mainLayout", {content: "foodEdit"});
+    BlazeLayout.render("adminLayout", {content: "foodEdit"});
+  }
+});
+FlowRouter.route('/admin/events',{
+  action: function(){
+    BlazeLayout.render("adminLayout", {content: "eventEdit"});
   }
 });
 FlowRouter.route('/admin/data',{
   action: function(){
-    BlazeLayout.render("mainLayout", {content: "siteData"});
+    BlazeLayout.render("adminLayout", {content: "siteData"});
   }
 });
--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Beer= new Mongo.Collection("beers");
 Food = new Mongo.Collection("food");
+Event= new Mongo.Collection("events");
 Meteor.startup(() => {
   // code to run on server at startup
 });
